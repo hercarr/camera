d22103d5c8cddfe7ac700bebbdae8948ccdf8e1c
hercarr <hercarr.mx@gmail.com>
2018-01-10 08:47:53 -0600

    se crea archivo para almacenar foto

--- app/src/main/java/mx/hercarr/camera/MainActivity.java
+++ app/src/main/java/mx/hercarr/camera/MainActivity.java
@@ -1,12 +1,21 @@
 package mx.hercarr.camera;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Button button;
+
+    private File photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +47,25 @@ public class MainActivity extends AppCompatActivity {
     private void showCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_CAMERA);
+            photo = createImageFile();
+            if (photo != null) {
+                Uri photoUri = FileProvider.getUriForFile(this, getPackageName(), photo);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent, REQUEST_CAMERA);
+            }
         }
+    }
+
+    private File createImageFile() {
+        String imageName = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.getDefault()).format(new Date());
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File photo = null;
+        try {
+            photo = File.createTempFile(imageName, ".jpg", storageDirectory);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return photo;
     }
 
 }
