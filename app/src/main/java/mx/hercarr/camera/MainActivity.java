e7bb85c5833c1e83a907256b146db178d6f0293b
hercarr <hercarr.mx@gmail.com>
2018-01-10 07:55:55 -0600

    se invoca camara del dispositivo

--- app/src/main/java/mx/hercarr/camera/MainActivity.java
+++ app/src/main/java/mx/hercarr/camera/MainActivity.java
@@ -1,13 +1,43 @@
 package mx.hercarr.camera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA = 1;
+
+    private ImageView imageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    private void init() {
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCamera();
+            }
+        });
+    }
+
+    private void showCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_CAMERA);
+        }
+    }
+
 }
